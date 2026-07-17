1e74be45de777ed09604afde1af4c06d328c566e
jwremoblas080 <jwremoblas08@gmail.com>
2026-07-18 00:25:16 +0800

    feat(database): verify migrations and database schema

---
+++ backend/database/migrations/2026_07_17_091207_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('supplier_id')->constrained('suppliers');
+            $table->string('product_name')->unique();
+            $table->string('sku')->unique();
+            $table->string('barcode')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('quantity')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
